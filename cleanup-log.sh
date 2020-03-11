f820f3a6e083fc11b3c17c40cdb37ac75f6aa9ce
amehthamilton <33587080+amehthamilton@users.noreply.github.com>
2020-03-11 17:18:22 -0400

    Update cleanup-log.sh

--- cleanup-log.sh
+++ cleanup-log.sh
@@ -10,32 +10,32 @@ E_XCD=86        # Can't change directory?
 E_NOTROOT=87    # Non-root exit error.
 
 # Run as root, of course.
-if [ "$UID" -ne "$ROOT_UID" ]
+if [ "${UID}" -ne "${ROOT_UID}" ]
 then
   echo "Must be root to run this script."
-  exit $E_NOTROOT
+  exit ${E_NOTROOT}
 fi
 
-if [ -n "$1" ]
+if [ -n "${1}" ]
 # Test whether command-line argument is present (non-empty).
 then
-  lines=$1
+  lines=${1}
 else
-  lines=$LINES # Default, if not specified on command-line.
+  lines=${LINES} # Default, if not specified on command-line.
 fi
 
-cd $LOG_DIR
+cd ${LOG_DIR}
 
 # Check if in the right directory before messing with the log files.
 if [ `pwd` != "$LOG_DIR" ]
 then
   echo "Can't change to $LOG_DIR."
-  exit $E_XCD
+  exit ${E_XCD}
 fi
 
 # Save the last section of messages log file.
 tail -n $lines messages > mesg.tmp
-mv mesg.temp messages
+mv mesg.tmp messages
 
 cat /dev/null > wtmp
 echo "Log files cleaned up."
